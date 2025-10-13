0b0fbe499bb594082d05fe029aeddd132ee7abe3
alejandrescobar <alemateu12354@gmail.com>
2025-10-13 18:28:28 -0300

    inicialização do modulo

---
+++ avaliação.v
@@ -0,0 +1,13 @@
+module principal (
+    input e1,
+    input e0, 
+    input p3, 
+    input p2,
+    input p1,
+    input p0,
+    output y1, 
+    output y0 
+);
+    assign y1 = e1 | (~e0 & p3) | (p2 & p1 & ~p0) | (p2 & p0 & ~p3); 
+    assign y2 = (e1 & ~p3) | (e1 & ~p2) | (e1 & p1 & p0) | (~e1 & e0) | (~e1 & p3); 
+endmodule
