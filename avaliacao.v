4387e8c64c33360ec8ef83da01246feb29e7be0d
alejandrescobar <alemateu12354@gmail.com>
2025-10-15 10:37:54 -0300

    implementação 7 seg

--- avaliacao.v
+++ avaliacao.v
@@ -8,7 +8,7 @@ module principal (
 
 //saidas normais
     output y1, 
-    output y0 
+    output y0 ;
 
 
 //display
@@ -30,6 +30,36 @@ module principal (
 
 
 
-    assign y1 = e1 | (~e0 & p3) | (p2 & p1 & ~p0) | (p2 & p0 & ~p3); 
-    assign y0 = (e1 & ~p3) | (e1 & ~p2) | (e1 & p1 & p0) | (~e1 & e0) | (~e1 & p3); 
+    assign y1 = y1_fio;
+    assign y0 = y0_fio;
+    assign {seg_a,seg_b,seg_c,seg_d,seg_e,seg_f,seg_g}=segmentos;
+
+    initial begin 
+        y1_fio=1'b0;//inicializamos os valores com 0 para nao dar erro de xxxxxxx lixo de memoria 
+        y0_fio=1'b0;
+        segmentos=7'b0000000;
+    end
+ always @(*) begin//sensivel a qualquer mudanca de entrada 
+        
+        
+       //bit mais significativo
+        y1_reg <= e1 | (~e0 & p3) | (p2 & p1 & ~p0) | (p2 & p0 & ~p3); 
+        
+        // bit menos significativo
+        y0_reg <= (e1 & ~p3) | (e1 & ~p2) | (e1 & p1 & p0) | (~e1 & e0) | (~e1 & p3);
+    end
+
+
+always @(*) begin
+ case ({y1_reg, y0_reg}) 
+2'b00: segmentos_reg <= 7'b0001100; 
+
+2'b01: segmentos_reg <= 7'b1111010;
+
+2'b10 segmentos_reg  <= 7'b1111100;
+
+2'b11 segmentos_reg  <= 7'b1110011;
+ endcase
+    end
+
 endmodule
