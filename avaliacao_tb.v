9861607c2f39bd0f0f377e5739e8d66b3ce0e6f9
Arthur Emanoel <arthur.correia@ufv.br>
2025-10-15 11:27:54 -0300

    Finalização do código

--- avaliacao_tb.v
+++ avaliacao_tb.v
@@ -2,7 +2,11 @@ module principal_tb;
  
     reg e1_in ,e0_in;
     reg p3_in,p2_in,p1_in,p0_in;
+
     wire y1_out,y0_out;
+    wire seg_a_out, seg_b_out, seg_c_out, seg_d_out;
+    wire seg_e_out, seg_f_out, seg_g_out;
+
     integer i,j;
 principal DUT (
 
@@ -13,7 +17,15 @@ principal DUT (
     .p2(p2_in),
     .p3(p3_in),
     .y1(y1_out),
-    .y0(y0_out)
+    .y0(y0_out),
+
+    .seg_a(seg_a_out),
+    .seg_b(seg_b_out),
+    .seg_c(seg_c_out),
+    .seg_d(seg_d_out),
+    .seg_e(seg_e_out),
+    .seg_f(seg_f_out),
+    .seg_g(seg_g_out)
 
 
 );
